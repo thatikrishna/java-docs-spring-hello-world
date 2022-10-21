3c8d09a2daf4c6c7fc07c8a45469f0404c77f091
thatikrishna <112326050+thatikrishna@users.noreply.github.com>
2022-10-21 19:02:12 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello World!";
+		return "Hello Krishna Chaithanya Thati! Keep doing good work. Keep Rocking!";
 	}
 }
